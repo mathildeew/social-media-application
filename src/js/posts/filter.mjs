79b6317f8564c02bd53c37d46d7c5805e870972a
mathildeew <mathildewiik@hotmail.com>
2022-11-30 16:46:35 +0100

    filter works and displays posts

--- src/js/posts/filter.mjs
+++ src/js/posts/filter.mjs
@@ -2,12 +2,31 @@ import { displayFeed } from "../home/displayFeed.mjs";
 
 export function filterPosts(posts) {
   const selectOne = document.querySelector("#selectOne");
+  const selectTwo = document.querySelector("#selectTwo");
+  const selectThree = document.querySelector("#selectThree");
 
   selectOne.addEventListener("click", (event) => {
     const oldestToNewest = posts.sort(function (a, b) {
       return a.id - b.id;
     });
-    // displayFeed(oldestToNewest);
+    displayFeed(oldestToNewest);
     // console.log(oldestToNewest);
+  });
+  selectTwo.addEventListener("click", (event) => {
+    const newestToOldest = posts.sort(function (a, b) {
+      return b.id - a.id;
+    });
+    displayFeed(newestToOldest);
+    // console.log(oldestToNewest);
+  });
+  selectThree.addEventListener("click", (event) => {
+    const postsWithImage = posts.filter((post) => {
+      if (post.media === null || post.media === "") {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    displayFeed(postsWithImage);
   });
 }
