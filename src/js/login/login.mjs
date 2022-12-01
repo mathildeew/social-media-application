49e09cc59dbe992e6c160a299bfd1f90464086cb
mathildeew <mathildewiik@hotmail.com>
2022-12-01 11:11:30 +0100

    login with errormessage and redirect

--- src/js/login/login.mjs
+++ src/js/login/login.mjs
@@ -15,24 +15,29 @@ export function loginUser() {
     const postContent = Object.fromEntries(formData.entries());
 
     async function loginAPI(url, postContent) {
-      const [getData, postData] = fetchOptions;
-      postData["body"] = JSON.stringify(postContent);
-      const response = await fetch(url, postData);
-      const json = await response.json();
+      try {
+        const [getData, postData] = fetchOptions;
+        postData["body"] = JSON.stringify(postContent);
+        const response = await fetch(url, postData);
+        const json = await response.json();
 
-      const name = json.name;
-      storage.set("name", name);
-      const avatar = json.avatar;
-      storage.set("avatar", avatar);
-      const accessToken = json.accessToken;
-      storage.set("token", accessToken);
-      console.log(json);
-      // if (json.errors[0].message === "Invalid email or password") {
-      //   errorMessage.style.display = "block";
-      // } else {
-      //   errorMessage.style.display = "none";
-      // }
-      window.location.href = "/profile/";
+        const name = json.name;
+        storage.set("name", name);
+        const avatar = json.avatar;
+        storage.set("avatar", avatar);
+        const accessToken = json.accessToken;
+        storage.set("token", accessToken);
+
+        if (accessToken === undefined) {
+          errorMessage.style.display = "block";
+        } else {
+          errorMessage.style.display = "none";
+
+          window.location.href = "/profile/";
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
     loginAPI(loginUrl, postContent);
   });
