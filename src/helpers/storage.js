8c45b817c7e11cb5e4e2321c09216e46206cd8c8
Megumi Kimura <kimuramegumi@gmail.com>
2023-01-20 23:05:09 +0100

    Cleans up unused files

--- src/helpers/storage.js
+++
@@ -1,6 +0,0 @@
-export default {
-  setItem: jest.fn((key, value) => (localStorage[key] = value)),
-  getItem: jest.fn((key) => localStorage[key]),
-  removeItem: jest.fn((key) => delete localStorage[key]),
-  clear: jest.fn(() => (localStorage = {})),
-};
